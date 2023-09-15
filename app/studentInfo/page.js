07b8681decf4a53f4b76b9e20ac7fcc801cec0d8
DiyaSharma15 <harry.diya161@gmail.com>
2023-09-15 10:31:11 -0600

    Student Info component Added

---
+++ app/studentInfo/page.js
@@ -0,0 +1,9 @@
+export default function StudentInfo(){
+    return(
+        <div>
+            <h2>Name: Diya Sharma</h2>
+            <h2>Course Section: cprg306-C</h2>
+            <Link href="https://github.com/DiyaSharma15/cprg306-assignments">Github Repository</Link>
+        </div>
+    );
+}
