37381469969242916b338a346d601dff8123464a
DiyaSharma15 <harry.diya161@gmail.com>
2023-09-15 20:07:26 -0600

    new page "week2" created

---
+++ app/week2/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+function heading(){
+    return(
+        <div>
+            <h1>My Shopping List</h1>
+            <Link href="/studentInfo"></Link>
+        </div>
+    )
+}
