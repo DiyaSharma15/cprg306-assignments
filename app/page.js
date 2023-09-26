e8ea854bd7601a952bc8aca22ac5343448e5fe2c
DiyaSharma15 <harry.diya161@gmail.com>
2023-09-25 23:00:15 -0600

    Created the main page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
-import StudentInfo from "./studentInfo/page"
+import StudentInfo from "./studentInfo";
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-  <div>
+  <main>
     <h1>CPRG 306: Web Development 2 - Assignments</h1>
     <StudentInfo />
-    <Link href="/week2">week2</Link>
-  </div>
+    <Link href="/app/week2">week2</Link>
+  </main>
   )
 }
--- app/week2/page.js
+++ app/week2/page.js
@@ -1,10 +1,11 @@
+import StudentInfo from "./studentInfo";
 import Link from "next/link";
 
 function heading(){
     return(
         <div>
             <h1>My Shopping List</h1>
-            <Link href="/studentInfo"></Link>
+            <StudentInfo />
         </div>
     )
 }
